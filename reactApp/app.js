dea78ee9a9d45ad2d7c9bcbce090088b3fa4843a
kyrakraft <kyraskraft@gmail.com>
2018-07-27 15:49:05 -0700

    add simple css

--- reactApp/app.js
+++ reactApp/app.js
@@ -5,8 +5,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 let dummyData = [
-  "a", "b", "c", "d"
+  {taskText: "a", completed: false}, {taskText: "b", completed: true}, {taskText: "c", completed: false}
 ]
 
 class TodoList extends React.Component{
@@ -23,12 +24,42 @@ class TodoList extends React.Component{
 class Todo extends React.Component{
 
   render(){
-    return(
-      <li> <button>X</button> {this.props.task} </li>
-    )
+    console.log(this.props.task.completed);
+
+return(
+  <div>
+    {this.props.task.completed ?
+        <li> <button className="x">X</button> {this.props.task.taskText} </li>
+     :
+      <li> <button className="x">X</button> <strike> {this.props.task.taskText} </strike> </li> }
+    </div>
+  )
+
   }
+}
+
+class InputLine extends React.Component{
+
+  render(){
+    return(
+    <div className="inputGroup">
+      <input type="text" value="asdf" className="newInput"/>
+      <button className="addButton">Add todo</button>
+    </div>
+
+  )}
+}
+
+class TodoApp extends React.Component{
+  render() {
+    return(
+    <div className="todoApp">
+      <InputLine/>
+      <TodoList/>
+    </div>
+  )}
 }
 
 
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
