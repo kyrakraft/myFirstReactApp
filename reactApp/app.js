2833313720028ea7f003c4d835d98580ee16895d
kyrakraft <kyraskraft@gmail.com>
2018-07-27 14:45:58 -0700

    create static mockup

--- reactApp/app.js
+++ reactApp/app.js
@@ -5,9 +5,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+let dummyData = [
+  "a", "b", "c", "d"
+]
+
+class TodoList extends React.Component{
+
+  render(){
+    return(
+      <ul>
+        {dummyData.map((item) => <Todo task={item}/>)}
+      </ul>
+    )
+  }
+}
+
+class Todo extends React.Component{
+
+  render(){
+    return(
+      <li> <button>X</button> {this.props.task} </li>
+    )
+  }
+}
 
 
 
-
-
-ReactDOM.render(<p>React lives!</p>, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
